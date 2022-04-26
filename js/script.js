c7b13dd0e9d72c816c070d8a1374041944dc8bf2
Igore4eg <viazminiv@gmail.com>
2022-04-26 17:48:36 +0300

    add fetch

--- js/script.js
+++ js/script.js
@@ -233,30 +233,29 @@ window.addEventListener('DOMContentLoaded', function() {
                 margin: 0 auto;
             `;
             form.insertAdjacentElement('afterend', statusMessage);
-        
-            const request = new XMLHttpRequest();
-            request.open('POST', 'server.php');
-            request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+
             const formData = new FormData(form);
 
             const object = {};
             formData.forEach(function(value, key){
                 object[key] = value;
             });
-            const json = JSON.stringify(object);
-
-            request.send(json);
-
-            request.addEventListener('load', () => {
-                if (request.status === 200) {
-                    console.log(request.response);
+           
+            fetch('server.php', {
+                method: 'POST',
+                headers: { 'Content-type': 'application/json; charset=utf-8' },
+                body: JSON.stringify(object)
+            })
+            .then(data => data.text())
+            .then(data => {
+                    console.log(data);
                     showThanksModal(message.success);
-                    form.reset();
                     statusMessage.remove();
-                } else {
+                })
+            .catch(() => {
                     showThanksModal(message.failure);
-                }
-            });
+            })
+            .finally(() => form.reset());
         });
     }
 
@@ -283,5 +282,15 @@ window.addEventListener('DOMContentLoaded', function() {
             prevModalDialog.classList.remove('hide');
             closeModal();
         }, 4000);
+
     }
+    // fetch('https://jsonplaceholder.typicode.com/posts', {
+    //     method: "POST",
+    //     body: JSON.stringify({name: 'Alex'}),
+    //     headers: {
+    //         'Content-type': 'application/json'
+    //     }
+    // })
+    //     .then(response => response.json())
+    //     .then(json => console.log(json));
 });
